567288e9226731716a09a88eef53965c4fa48541
taheerabdul1 <taheerabdul1@gmail.com>
2024-04-15 14:10:01 +0100

    "Update user store with initial state and refactor login/logout actions"

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,104 +1,96 @@
-  import { defineStore } from "pinia";
-  import router from "../router/index";
-  // import { watch } from "vue";
+import { defineStore } from "pinia";
+import router from "../router/index";
 
+const initialState = {
+  _id: 0,
+  username: "",
+  name: "",
+  email: "",
+  password: "",
+  isAdmin: false,
+  chosenMosque: "",
+  isLoggedIn: false,
+};
 
-  export const useUserStore = defineStore("user", {
-    state: () => {
-      const storedState = localStorage.getItem("user");
-      if (storedState) {
-        // parse the stored state and return it
-        return JSON.parse(storedState);
-      } else {
-        // return the initial state
-        return {
-          _id: 0,
-          username: "",
-          name: "",
-          email: "",
-          password: "",
-          isAdmin: false,
-          chosenMosque: "",
-          isLoggedIn: false,
-        };
-      }
-    },
-    actions: {
-      async loggedIn() {
-        try {
-          const response = await fetch("/api/loggedIn", {
-            method: "GET",
-            credentials: "include",
-          });
-          const data = await response.json();
-          if (data.success) {
-            this._id = data._id;
-            this.isLoggedIn = true;
-            this.username = data.username;
-            this.name = data.name;
-            this.email = data.email;
-            this.isAdmin = data.isAdmin;
-            this.chosenMosque = data.chosenMosque;
-          } else {
-            this.isLoggedIn = false;
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      },
-      async logout() {
-        const response = await fetch("/api/logout", {
+export const useUserStore = defineStore("user", {
+  state: () => {
+    const storedState = localStorage.getItem("user");
+    return storedState ? JSON.parse(storedState) : initialState;
+  },
+  actions: {
+    async loggedIn() {
+      try {
+        const response = await fetch("/api/loggedIn", {
           method: "GET",
           credentials: "include",
         });
         const data = await response.json();
         if (data.success) {
-          this._id = 0;
-          this.username = "";
-          this.name = "";
-          this.email = "";
-          this.password = "";
+          this._id = data._id;
+          this.isLoggedIn = true;
+          this.username = data.username;
+          this.name = data.name;
+          this.email = data.email;
+          this.isAdmin = data.isAdmin;
+          this.chosenMosque = data.chosenMosque;
+          localStorage.setItem("user", JSON.stringify(this.$state));
+        } else {
           this.isLoggedIn = false;
-          this.isAdmin = false;
+          localStorage.removeItem("user");
         }
-      },
-      async login() {
-        try {
-          const response = await fetch("/api/login", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              username: this.username,
-              password: this.password,
-            }),
-          });
-          if (response.status === 401) {
-            alert("Wrong Username or Password");
-            router.push("/login");
-          } else {
-            const data = await response.json();
-            if (data.success) {
-              alert("Login successful");
-              this._id = data._id;
-              this.username = data.username;
-              this.name = data.name;
-              this.email = data.email;
-              this.isAdmin = data.isAdmin;
-              this.chosenMosque = data.chosenMosque;
-              this.loggedIn();
-              router.push("/");
-            }
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      },
+      } catch (e) {
+        console.log(e);
+      }
     },
-  });
+    async logout() {
+      const response = await fetch("/api/logout", {
+        method: "GET",
+        credentials: "include",
+      });
+      const data = await response.json();
+      if (data.success) {
+        Object.assign(this, initialState);
+        localStorage.removeItem("user");
+      }
+    },
+    async login() {
+      try {
+        const response = await fetch("/api/login", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password,
+          }),
+        });
+        if (response.status === 401) {
+          alert("Wrong Username or Password");
+          router.push("/login");
+        } else {
+          const data = await response.json();
+          if (data.success) {
+            alert("Login successful");
+            this._id = data._id;
+            this.username = data.username;
+            this.name = data.name;
+            this.email = data.email;
+            this.isAdmin = data.isAdmin;
+            this.chosenMosque = data.chosenMosque;
+            this.loggedIn();
+            router.push("/");
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+});
 
+  // import { watch } from "vue";
   // const userStore = useUserStore();
 
   // watch(
